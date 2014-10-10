2925f4e4df14c2904da27fb3e82a915072e1b6e2
Filipe Batista <me@filipeb.com>
2014-10-10 15:35:13 +0100

    file extensions

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Venda de Garagem</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="fb-root"></div>
+   
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Venda de Garagem</h1>
+        <p>Descontos até <strong>75%</strong>!</p>
+      </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <h3 class="col-md-12 highlight">1. Selecciona os artigos que te interessam</h3>
+        </div>
+        <div class="row">
+            <h4 class="col-md-12 divider"><span>Electrodomésticos</span></h4>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="aspirador">
+                    <div class="item-discount">-15%</div>
+                    <img src="img/aspirador.jpg" alt="Aspirador">
+                    <div class="item-title">Aspirador a água Ufesa</div>
+                    <div class="item-price">Preço de amigo <span>153&euro;</span></div>
+                    <div class="item-price item-price-old">P.V.P <span>179.99&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Marca:</strong> UFESA<br><strong>Modelo:</strong> <a href='https://www.worten.pt/inicio/pequenos-eletrodomesticos/limpeza-de-pavimentos/aspiradores/aspirador-ufesa-ap8110.html' target='_blank'>AP8110</a><br><strong>Estado:</strong> Como novo<br><strong>Data de compra:</strong> < 1 ano<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="iogurteira">
+                    <img src="img/iogurteira.jpg" alt="">
+                    <div class="item-title">Iogurteira Braun</div>
+                    <div class="item-price">Preço de amigo <span>15&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Cor:</strong> Branco<br><strong>Marca:</strong> Braun<br><br><strong>Estado:</strong> Como novo<br><strong>Numero de copos</strong> > 6 copos<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+            <h4 class="col-md-12 divider"><span>Som</span></h4>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="movel-sala">
+                    <div class="item-discount">-20%</div>
+                    <img src="img/speakers.jpg" alt="Besta">
+                    <div class="item-title">Colunas Creative 7.1</div>
+                    <div class="item-price">Preço de amigo <span>220&euro;</span></div>
+                    <div class="item-price item-price-old">P.V.P <span>275&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Marca:</strong> IKEA<br><strong>Modelo:</strong> BESTÅ<br><strong>Cor:</strong> Branco / Verde<br><strong>Composição:</strong> 4 gavetas, 4 prateleiras com porta, 2 prateleiras vidro<br><strong>Dimensões:</strong> 192 x 120 cm<br><strong>Estado:</strong> Como novo<br><strong>Data de compra:</strong> < 1 ano">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="headphones">
+                    <div class="item-discount">-50%</div>
+                    <img src="img/headphones.jpg" alt="Sofá">
+                    <div class="item-title">Headphones Sony</div>
+                    <div class="item-price">Preço de amigo <span>10&euro;</span></div>
+                    <div class="item-price item-price-old">P.V.P <span>20&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Cor:</strong> Branco<br>Sinais de uso nas esponjas<br><strong>Data de compra:</strong> 2 anos<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+            <h4 class="col-md-12 divider"><span>Informática</span></h4>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="caixa-pc">
+                    <div class="item-discount">-75%</div>
+                    <img src="img/thermaltake.jpg" alt="Mosjo">
+                    <div class="item-title">Caixa PC Thermaltake</div>
+                    <div class="item-price">Preço de amigo <span>25&euro;</span></div>
+                    <div class="item-price item-price-old">P.V.P <span>100&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Marca:</strong> Thermaltake<br><strong>Modelo:</strong> Tsunami</a><br><strong>Estado:</strong> Está uma ventuinha danificada<br><strong>Data de compra:</strong> 2005<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="caixa-pc">
+                    <div class="item-discount">-75%</div>
+                    <img src="img/hfx.jpg" alt="PS 2012">
+                    <div class="item-title">Caixa HTPC Passiva</div>
+                    <div class="item-price">Preço de amigo <span>35&euro;</span></div>
+                    <div class="item-price item-price-old">P.V.P <span>140&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Marca:</strong> HFX<br><strong>Estado:</strong> Como novo<br><strong>Data de compra:</strong> 2006<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="volante">
+                    <img src="img/volante.gif" alt="Volante">
+                    <div class="item-title">Microsoft Precision Wheel</div>
+                    <div class="item-price">Preço de amigo <span>15&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Marca:</strong> Microsoft<br><strong>Estado:</strong> Como novo<br><strong>Data de compra:</strong> 2004<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="colunas-genius">
+                    <img src="img/colunas-genius.jpg" alt="Colunas">
+                    <div class="item-title">Colunas Genius</div>
+                    <div class="item-price">Preço de amigo <span>15&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Marca:</strong> Genius<br><strong>Estado:</strong> Como novo<br><strong>Data de compra:</strong> 2003<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+
+            <h4 class="col-md-12 divider"><span>Outros</span></h4>
+            <div class="col-sm-6 col-md-4">
+                <div class="item" data-item="candeeiro">
+                    <div class="item-discount">-20%</div>
+                    <img src="img/lamp.jpg" alt="Candeeiro">
+                    <div class="item-title">Candeeiro</div>
+                    <div class="item-price">Preço de amigo <span>2.5&euro;</span></div>
+                    <div class="item-price item-price-old">P.V.P <span>2.99&euro;</span></div>
+                    <button class="btn btn-success js-extra-info" data-container="body" data-toggle="popover" title="Informações adicionais" data-content="<strong>Cor:</strong> Vermelho<br><strong>Marca:</strong> IKEA<br><strong>Modelo:</strong> <a href='http://www.ikea.com/pt/pt/catalog/products/20046988/' target='_blank'>Lampan</a><br><strong>Estado:</strong> Como novo<br><strong>Data de compra:</strong> < 2 anos<br>">Detalhes</button>
+                    <button class="btn btn-success btn-sm js-interested">Adicionar</button>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="section-2">
+            <div class="row">
+                <h3 class="col-md-12 highlight">2. Deixa-nos os teus dados de contacto</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <form class="form-horizontal js-form" action="/mail.php" id="contact-form" role="form" novalidate>
+                        <fieldset disabled>
+                          <div class="form-group">
+                            <label for="nome" class="col-sm-2 control-label">Nome</label>
+                            <div class="col-sm-10 form-input">
+                              <input type="text" class="form-control" name="form_nome" id="nome" placeholder="Para te podermos identificar" data-validation="required">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="email" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10 form-input">
+                              <input type="email" class="form-control" name="form_email" id="email" placeholder="Não vale colocar o email de outra pessoa!" data-validation="required email">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="notes" class="col-sm-2 control-label">Notas</label>
+                            <div class="col-sm-10 form-input">
+                              <textarea class="form-control" style="resize: none;" name="form_notes" id="notes" placeholder="Caso queiras colocar algum comentário adicional"></textarea>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <button type="submit" name="form_submit" class="btn btn-success">Enviar</button>
+                            </div>
+                          </div>
+                        </fieldset>
+                    </form>
+                    <p class="note">Os preços dos artigos já têm um desconto considerável face ao seu valor de compra, mas dependendo das situações podemos rever o valor dos mesmos. Se estiveres realmente interessado em algum dos artigos, faz o pedido para ficar registado e deixa um comentário na zona das notas.</p>
+                </div>
+                <div class="col-md-3 col-md-offset-1">
+                    <strong>Lista de Artigos</strong>
+                    <ul class="item-list">
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="form-answer">
+            <div class="row">
+                <h3 class="col-md-12 highlight">3. Obrigado pelo teu interesse!</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Em breve receberás a nossa resposta para tratarmos de todos os detalhes. Entretanto se nos quiseres ajudar a divulgar esta página, partilha com os teus amigos nas redes sociais.</p> 
+                    <div class="social">
+                        <div class="fb-share-button" data-href="http://connectcoimbra.com/sale/" data-type="button"></div>
+                        <a href="https://twitter.com/share" class="twitter-share-button" data-lang="pt" data-count="none" data-dnt="true">Tweetar</a>
+                        <button class="btn btn-success btn-xs js-startover">Começar de novo</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.1.47/jquery.form-validator.min.js"></script>
+    <script>
+        var itemsCount = 0;
+
+        $(document).ready(function(){
+            $('.js-extra-info').popover({
+              trigger: 'focus',
+              html: true,
+              placement: 'right'
+            });
+
+            $('.js-startover').on('click', function(e){
+                location.reload();
+                e.preventDefault();
+            });
+
+            $('.js-interested').on('click', function(e){
+                $button = $(this);
+                $parent = $button.parents('.item:first');
+                $list = $('.item-list');
+
+                if ($parent.hasClass('item-selected')){ // remove item
+                    $parent.removeClass('item-selected');
+                    $button.addClass('btn-success').html('Estou interessado');
+                    $list.find("li[data-item='" + $parent.data('item') + "']").remove();
+                    itemsCount--;
+                } else { // Add item
+                    $parent.addClass('item-selected');
+                    $button.removeClass('btn-success').html('Retirar interesse');
+                    $list.append('<li data-item="' + $parent.data('item') + '">&mdash; <span>' + $parent.find('.item-title').html() + '</span></li>');
+                    itemsCount++;
+                }
+
+                if (itemsCount > 0) {
+                    $('#contact-form fieldset').prop('disabled',false);
+                } else {
+                    $('#contact-form fieldset').prop('disabled',true);
+                }
+
+                e.preventDefault();
+            });
+
+            var submitForm = function(form){
+                myData = form.serialize() ;
+
+                form.find('fieldset').prop('disabled',true);
+                form.find('button').html('A enviar...');
+                $('.item .btn').prop('disabled', true);
+
+                itemList = $('.item-list li').map(function(){
+                    return 'items[]=' + encodeURIComponent($(this).find('span').html());
+                }).get();
+                myData += '&' + itemList.join('&');
+
+                $.ajax({
+                    type: "POST",
+                    url: "mail.php",
+                    data: myData
+                })
+                .done(function( ) {
+                    $('.form-answer').fadeIn();
+                    form.find('button').html('Enviado!');
+                    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+                });
+            };
+
+            var myLanguage = {
+              errorTitle : 'Form submission failed!',
+              requiredFields : 'O campo é de preenchimento obrigatório',
+              badEmail : 'O e-mail não tem o formato correcto'
+            };
+
+            $.validate({ 
+                form: '.js-form',
+                language: myLanguage,
+                onSuccess : function() {
+                    submitForm($('.js-form'));
+
+                    return false; 
+                }
+            });
+        });
+    </script>
+     <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/pt_PT/sdk.js#xfbml=1&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</body>
+</html>
